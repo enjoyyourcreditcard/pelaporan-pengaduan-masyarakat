14183e43eb8e051252aa05f6d9677d70a603767b
enjoyyourcreditcard <joywidiwibowo@gmail.com>
2023-02-17 00:51:08 +0700

    :heavy_plus_sign: Flowbite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
     <!-- Scripts -->
     @viteReactRefresh
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body>
     <div id="app">
